c96d576408177e8500b3518c21c2d9ddd68a9164
NnEo0 <jfollana@tumiaqp.com>
2024-02-19 22:09:17 -0500

    terminado

--- web/static/js/inicio.js
+++ web/static/js/inicio.js
@@ -1,25 +1,260 @@
-import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r132/build/three.module.js';
+//import * as THREE from '/static/js/node_modules/three/build/three.module.js';
+//import { GLTFLoader } from 'static/js/node_modules/three/addons/loaders/GLTFLoader.js';
+// Resto del código...
 
-function init() {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.getElementById('scene-container').appendChild(renderer.domElement);
+const canvas= document.querySelector('canvas.webgl')
 
-    // Agrega tu escena y objetos Three.js aquí
+/**
+ * Loaders
+ */
+const loadingBarElement = document.querySelector('.loading-bar')
+const bodyElement = document.querySelector('body')
+const loadingManager = new THREE.LoadingManager(
+    () => {
+        window.setTimeout(() => {
+            gsap.to(overlayMaterial.uniforms.uAlpha, {
+                duration: 3,
+                value: 0,
+                delay: 1
+            })
+            gsap.to(overlayMaterial.uniforms.uAlpha, {
+                duration: 3,
+                value: 0,
+                delay: 1
+            })
 
-    camera.position.z = 5;
+            loadingBarElement.classList.add('ended')
+            bodyElement.classList.add('loaded')
+            loadingBarElement.style.transform = ''
 
-    const animate = function () {
-        requestAnimationFrame(animate);
+        }, 500)
+    },
+    (itemUrl, itemsLoaded, itemsTotal) => {
+        console.log(itemUrl, itemsLoaded, itemsTotal)
+        const progressRatio = itemsLoaded / itemsTotal
+        loadingBarElement.style.transform = `scaleX(${progressRatio})`
+        console.log(progressRatio)
+    },
+    () => {
 
-        // Actualiza tu animación aquí
+    }
+)
+const gltfLoader = new THREE.GLTFLoader(loadingManager)
+/**
+ *  Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+const alphaShadow = textureLoader.load('/static/glb/simpleShadow.jpg');
 
-        renderer.render(scene, camera);
-    };
+// Scene
+const scene = new THREE.Scene()
 
-    animate();
+const sphereShadow = new THREE.Mesh(
+    new THREE.PlaneGeometry(1.5, 1.5),
+    new THREE.MeshBasicMaterial({
+        transparent: true,
+        color: 0x000000,
+        opacity: 0.5,
+        alphaMap: alphaShadow
+    })
+)
+
+sphereShadow.rotation.x = -Math.PI * 0.5
+
+sphereShadow.position.y = -1
+sphereShadow.position.x = 1.5;
+
+scene.add(sphereShadow)
+/**
+ * Overlay
+ */
+const overlayGeometry = new THREE.PlaneGeometry(2, 2, 1, 1)
+const overlayMaterial = new THREE.ShaderMaterial({
+    vertexShader: `
+        void main() {
+            gl_Position = vec4(position, 1.0);
+        }
+    `,
+    fragmentShader: `
+        uniform float uAlpha;
+        void main() {
+            gl_FragColor = vec4(0.0, 0.0, 0.0, uAlpha);
+            // gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+        }
+    `,
+    uniforms: {
+        uAlpha: {
+            value: 1.0
+        }
+    },
+    transparent: true
+})
+const overlay = new THREE.Mesh(overlayGeometry, overlayMaterial);
+scene.add(overlay)
+
+
+/**
+ * GLTF Model
+ */
+let donut = null
+
+gltfLoader.load(
+    '/static/glb/LogoInkarobot.glb',
+    (gltf) => {
+        console.log(gltf);
+
+        donut = gltf.scene
+
+        const radius = 0.04
+
+        donut.position.x = 1.5;
+
+        donut.rotation.x = Math.PI * 0.3
+        donut.rotation.z = Math.PI * 0.15
+
+        donut.scale.set(radius, radius, radius)
+
+        scene.add(donut)
+    },
+    (progress) => {
+        console.log(progress);
+    },
+    (error) => {
+        console.error(error);
+    }
+)
+
+/**
+ * Light
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
+directionalLight.position.set(1, 2, 0)
+
+directionalLight.castShadow = true
+scene.add(directionalLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
 }
 
-window.onload = init;
+/**
+ * Scroll
+ */
+let scrollY = window.scrollY
+let currentSection = 0
+
+const transformDonut = [{
+        rotationZ: 0.45,
+        positionX: 1.5
+    },
+    {
+        rotationZ: -0.45,
+        positionX: -1.5
+    },
+    {
+        rotationZ: 0.0314,
+        positionX: 0
+    },
+    {
+        rotationZ: 0.0314,
+        positionX: 0
+    },
+]
+
+window.addEventListener('scroll', () => {
+
+    scrollY = window.scrollY
+    const newSection = Math.round(scrollY / sizes.height)
+
+    console.log(newSection);
+
+    if (newSection != currentSection) {
+        currentSection = newSection
+
+        if (!!donut) {
+            gsap.to(
+                donut.rotation, {
+                    duration: 1.5,
+                    ease: 'power2.inOut',
+                    z: transformDonut[currentSection].rotationZ
+                }
+            )
+            gsap.to(
+                donut.position, {
+                    duration: 1.5,
+                    ease: 'power2.inOut',
+                    x: transformDonut[currentSection].positionX
+                }
+            )
+
+            gsap.to(
+                sphereShadow.position, {
+                    duration: 1.5,
+                    ease: 'power2.inOut',
+                    x: transformDonut[currentSection].positionX - 0.2
+                }
+            )
+        }
+    }
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 1000)
+camera.position.z = 5
+
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true,
+    alpha: true
+})
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+let lastElapsedTime = 0
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - lastElapsedTime
+    lastElapsedTime = elapsedTime
+
+    if (!!donut) {
+        donut.position.y = Math.sin(elapsedTime * .5) * .1 - 0.1
+        sphereShadow.material.opacity = (1 - Math.abs(donut.position.y)) * 0.3
+    }
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
+
+/**
+ * On Reload
+ */
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
